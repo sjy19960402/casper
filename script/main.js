eb4c9f8b8100999fd2e6895d159b8cfac43f47de
shin jinyoung <100466973+sjy19960402@users.noreply.github.com>
2022-04-20 22:16:44 +0900

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,55 +1,56 @@
 $(document).ready(function(){
-    
-    // 헤더영역에 오버하면 .on, .h_on 클래스 적용, 마우스 아웃시 제거
 
-    $('header').hover(function(){ //오버시
+  // 사용자가 마우스로 헤더영역에 오버하면 .on, .h_on을 적용하고 마우스를 아웃하면 .on, .h_on을 제거한다.
+
+  $('header').hover(function(){ //오버시 동작
+    $(this).find('i.fas').addClass('on');
+    $(this).find('.gnb a').addClass('on');
+    //검정색로고
+    $(this).find('h1 img').attr('src','./images/logo-casper.black.png');
+    $(this).addClass('h_on');
+  },function(){ //아웃시 동작
+    $('header i.fas').removeClass('on');
+    $('.gnb a').removeClass('on');
+     //흰색로고
+    $('h1 img').attr('src','./images/logo-casper-white.png');
+    $(this).removeClass('h_on');
+  });
+
+  // 스크롤이벤트를 사용하여 일정영역이상으로 내려가면 위 서식을 적용한다.
+  $(window).scroll(function(){
+    let sPos = $(this).scrollTop();
+    console.log(sPos);//스크롤값을 체크한다.
+    
+    if(sPos>=953){ //영상콘텐츠가 모두 올라가면
+      //만약에 사용자가 헤더에 마우스를 아웃하면 서식이 지워지지 않는다.
+      $('header').find('i.fas').addClass('on');
+      $('header').find('.gnb a').addClass('on');
+      //검정색로고
+      $('header').find('h1 img').attr('src','./images/logo-casper.black.png'); 
+      $('header').addClass('h_on');
+
+      $('header').mouseleave(function(){ //마우스 아웃시 동작
         $(this).find('i.fas').addClass('on');
         $(this).find('.gnb a').addClass('on');
-        //로고 이미지 변경
-        $(this).find('h1 img').attr('src','./img/logo_black.png');
-        $('header').addClass('h_on');
+        //검정색로고
+        $(this).find('h1 img').attr('src','./images/logo-casper.black.png');
+        $(this).addClass('h_on');
+      });
+    }else{
+      $('header i.fas').removeClass('on');
+      $('.gnb a').removeClass('on');
+       //흰색로고
+      $('h1 img').attr('src','./images/logo-casper-white.png');
+      $('header').removeClass('h_on');
 
-    },function(){ //아웃시
-        $('header i.fas').removeClass('on');
-        $('.gnb a').removeClass('on');
-        $('h1 img').attr('src','./img/logo_white.png');
-        $('header').removeClass('h_on');
-    });
+      $('header').mouseleave(function(){ //마우스 아웃시 동작
+        $(this).find('i.fas').removeClass('on');
+        $(this).find('.gnb a').removeClass('on');
+        //검정색로고
+        $(this).find('h1 img').attr('src','./images/logo-casper-white.png');
+        $(this).removeClass('h_on');
+      });
+    }
+  });
 
-    // 스크롤 이벤트, 일정영역 이상 내려가면 위 서식 적용
-    $(window).scroll(function(){
-        let sPos = $(this).scrollTop();
-        console.log(sPos);
-        if(sPos>=953){
-            // 헤더에 마우스를 오버해도 서식이 사라지지 않도록
-            
-            $('header').find('i.fas').addClass('on');
-            $('header').find('.gnb a').addClass('on');
-            //로고 이미지 변경
-            $('header').find('h1 img').attr('src','./img/logo_black.png');
-            $('header').addClass('h_on');
-
-            $('header').mouseleave(function(){
-                $(this).find('i.fas').addClass('on');
-                $(this).find('.gnb a').addClass('on');
-                //로고 이미지 변경
-                $('header').find('h1 img').attr('src','./img/logo_black.png');
-                $('header').addClass('h_on');
-            });
-
-        }else{
-        $('header i.fas').removeClass('on');
-        $('.gnb a').removeClass('on');
-        $('h1 img').attr('src','./img/logo_white.png');
-        $('header').removeClass('h_on');
-
-        $('header').mouseleave(function(){
-            $(this).find('i.fas').removeClass('on');
-            $(this).find('.gnb a').removeClass('on');
-            //로고 이미지 변경
-            $('header').find('h1 img').attr('src','./img/logo_white.png');
-            $('header').removeClass('h_on');
-        });
-        }
-    });
 });
